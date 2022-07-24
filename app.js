df2c1fcab02a82a3ebe778af3abe1d44d8e5700a
lebarisunday80 <lebarisunday80@gmail.com>
2022-07-24 21:38:12 +0100

    newcommit

---
+++ app.js
@@ -0,0 +1,56 @@
+const change = document.getElementById("sunset1");
+
+change.addEventListener("click",(event) =>{
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+
+});
+
+const change2 = document.getElementById("sunset2");
+
+change2.addEventListener("click",(event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "red";
+});
+
+const change3 = document.getElementById("sunset3");
+
+change3.addEventListener("click",(event) =>{
+    event.target.style.background = "blue";
+    event.target.style.color = "black";
+});
+
+const change4 = document.getElementById("sunset4");
+
+change4.addEventListener("click",(event) =>{
+    event.target.style.background = "brown";
+    event.target.style.color = "yellow";
+});
+
+const change5 = document.getElementById("sunset5");
+
+change5.addEventListener("click",(event) =>{
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("sunset6");
+
+change6.addEventListener("click",(event) =>{
+    event.target.style.background = "green";
+    event.target.style.color = "red";
+});
+
+const change7 = document.getElementById("sunset7");
+
+change7.addEventListener("click",(event) =>{
+    event.target.style.background = "purple";
+    event.target.style.color = "white";
+});
+
+const change8 = document.getElementById("sunset8");
+
+change8.addEventListener("click",(event) =>{
+    event.target.style.background = "grey";
+    event.target.style.color = "red";
+});
